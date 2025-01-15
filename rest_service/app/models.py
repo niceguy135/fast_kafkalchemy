498801c2001a0c3e6d60b65ba546e3ee3a273f13
Константин <danov@ancud.local>
2025-01-16 00:35:07 +0300

    models.py(add): создал модель завки пользователя

---
+++ rest_service/app/models.py
@@ -0,0 +1,38 @@
+import datetime
+from typing import Annotated
+
+from sqlalchemy import String, text
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+str_100 = Annotated[str, String(100)]
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("now()"))]
+
+
+class BaseModel(DeclarativeBase):
+    """
+    Абстрактный класс модели
+    """
+
+    repr_cols_num = 3
+    repr_cols = tuple()
+
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
+
+
+class ApplicationModel(BaseModel):
+    """
+    Модель заявки (таблица applications)
+    """
+    __tablename__ = "applications"
+
+    id: Mapped[intpk]
+    user_name: Mapped[str_100]
+    description: Mapped[str]
+    created_at: Mapped[created_at]
