342df3937f61d5e5335aa367ca7ccc1f159e9906
Константин <danov@ancud.local>
2025-01-16 01:56:22 +0300

    applications.py(add): реализовал логику работы энпоинта выдачи заявок с пагинацией

--- rest_service/app/api/routes/applications.py
+++ rest_service/app/api/routes/applications.py
@@ -1,16 +1,36 @@
 from typing import Any
 
 from fastapi import APIRouter
+from fastapi_pagination import Page, paginate
+
+from sqlalchemy import select
+
+from app.core.datebase import async_session_factory
+from app.models import ApplicationModel
+from app.dto_schemas import ApplicationDTO
+
 
 router = APIRouter(prefix="/applications", tags=["apps"])
 
 
 @router.get("/", tags=["get_apps"])
-def get_apps() -> Any:
+async def get_apps(username: str = None) -> Page[ApplicationDTO]:
     """
     Получить список заявок от всех пользователей или от конкретного пользователя
     """
-    pass
+    async with async_session_factory() as session:
+        if username is None:
+            query = select(ApplicationModel)
+        else:
+            query = (
+                select(ApplicationModel)
+                .where(ApplicationModel.user_name == username)
+            )
+
+        query_result = await session.execute(query)
+        achievements = query_result.scalars().all()
+
+    return paginate([ApplicationDTO.model_validate(row, from_attributes=True) for row in achievements])
 
 
 @router.post("/", tags=["create_app"])
