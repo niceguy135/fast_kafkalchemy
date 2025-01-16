a909af9509084c9ec43329916e34c1c1608bcfee
Данов Константин <danov@ancud.local>
2025-01-16 17:39:51 +0300

    datebase.py(add): добавил синхронный движок и завод сессий

--- rest_service/app/core/datebase.py
+++ rest_service/app/core/datebase.py
@@ -1,8 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app.core.config import settings
 
 
+# для синхронной работы
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg
+)
+sync_session_factory = sessionmaker(sync_engine)
+
+# для асинхронной работы
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg
 )
