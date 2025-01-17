f68c8cddf18bb58496278fbac791fe5715404fda
Данов Константин <danov@ancud.local>
2025-01-17 15:53:34 +0300

    добавил в файл и класс конфигов ip и порт kafka сервиса

--- rest_service/app/core/config.py
+++ rest_service/app/core/config.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    KAFKA_IP: str
+    KAFKA_PORT: int
+
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
