71d4179f14e7378adf640375d3756e19e581413e
Константин <danov@ancud.local>
2025-01-20 17:07:26 +0300

    rest_prestart.py(add): добавил коментов; форматнул по pep8

--- rest_service/app/rest_prestart.py
+++ rest_service/app/rest_prestart.py
@@ -1,9 +1,9 @@
 import logging
 
 from sqlalchemy import select
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 from app.core.datebase import sync_session_factory
-from tenacity import retry, stop_after_attempt, wait_fixed
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,8 +17,14 @@ wait_seconds = 1
     wait=wait_fixed(wait_seconds),
 )
 def check_connection() -> None:
+    """
+    Банальная проверка на возможность отправки запрос в базу данных (просто пытаемся вызвать ``SELECT 1`` у БД)
+
+    Проверка проводиться либо пока не будет достигнут успех, либо пока количество проверок не достигнет ``max_tries``
+    :return: None
+    """
     try:
-        with sync_session_factory as session:
+        with sync_session_factory() as session:
             session.exec(select(1))
     except Exception as e:
         logger.error(e)
