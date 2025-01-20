30aada524495caab7819437a345ccd5e57411684
Константин <danov@ancud.local>
2025-01-20 16:34:53 +0300

    dto_schemas.py(add): добавил дополнительных свойств полям для правильной валидации post запроса

--- rest_service/app/dto_schemas.py
+++ rest_service/app/dto_schemas.py
@@ -1,13 +1,13 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ApplicationAddDTO(BaseModel):
     """
     Схема данных для создания запроса от пользователя
     """
-    user_name: str
-    description: str
+    user_name: str = Field(...,min_length=5, max_length=50)
+    description: str = Field(..., max_length=150)
 
 
 class ApplicationDTO(ApplicationAddDTO):
